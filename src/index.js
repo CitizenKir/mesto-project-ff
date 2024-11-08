c6a39f5845978e8cb4f9f9930e493afc01471456
kirill <k.omelianenko@quickresto.ru>
2024-11-08 14:03:07 +0500

    fix: Правки ревьюера пр6

--- src/index.js
+++ src/index.js
@@ -6,51 +6,63 @@ import { openPopup, closePopup} from "./components/popup";
 const cardTemplate = document.querySelector("#card-template").content
 const placesList = document.querySelector(".places__list")
 
+//попап редактирования профиля
 const profileEditButton = document.querySelector(".profile__edit-button")
 const profileEditPopup = document.getElementById('profile_edit')
+const profileEditCloseButton = profileEditPopup.querySelector('.popup__close')
 
+//попап создания карточки
 const profileAddButton = document.querySelector(".profile__add-button")
 const profileAddPopup = document.getElementById('profile_add')
+const profileAddCloseButton = profileAddPopup.querySelector('.popup__close')
 
+//попап с детальным изображением
 const imageDetail = document.getElementById('image_detail')
+const imageDetailCloseButton = imageDetail.querySelector('.popup__close')
+const imageElement = imageDetail.querySelector('.popup__image')
+const imageCaption = imageDetail.querySelector('.popup__caption')
 
+//форма редактирования профиля внутри попапа
 const formProfileEdit = document.forms.profileEdit
 const formAddCard = document.forms.newPlace
 
+//инпуты внутри формы редактирования профиля
 const nameInput = formProfileEdit.name
 const jobInput = formProfileEdit.description
 
-let profileName = document.querySelector(".profile__title")
-let profileDescription = document.querySelector(".profile__description")
+//элементы имени и описания профиля на странице, текстовый контент которых может быть перезаписан в форме
+const profileName = document.querySelector(".profile__title")
+const profileDescription = document.querySelector(".profile__description")
+
 nameInput.value = profileName.textContent
 jobInput.value = profileDescription.textContent
 
 const renderCard = (cardInfo) => {
     const cardElement = createCard(cardTemplate, cardInfo, deleteCard, handleLike, openImage)
 
-    return placesList.prepend(cardElement)
+    placesList.prepend(cardElement)
 }
 
 const overlayClickHandler = (evt) => {
     if (evt.target.classList.contains('popup')) {
-       return closePopup()
+        const activePopup = document.querySelector(".popup_is-opened")
+       closePopup(activePopup)
     }
 }
 
 const keyHandler = (evt) => {
     if (evt.code === 'Escape') {
-       return closePopup()
+        const activePopup = document.querySelector(".popup_is-opened")
+        closePopup(activePopup)
     }
 }
 
 const openImage = (evt) => {
-    const imageElement = imageDetail.querySelector('.popup__image')
-    const imageCaption = imageDetail.querySelector('.popup__caption')
     imageElement.src = evt.target.currentSrc
     imageElement.alt = evt.target.alt
     imageCaption.textContent = evt.target.alt
 
-   return openPopup(imageDetail, overlayClickHandler, keyHandler)
+    openPopup(imageDetail, imageDetailCloseButton, overlayClickHandler, keyHandler)
 }
 
 const handleProfileEdit = (evt) => {
@@ -58,7 +70,7 @@ const handleProfileEdit = (evt) => {
     profileName.textContent = nameInput.value
     profileDescription.textContent = jobInput.value
 
-    closePopup()
+    closePopup(profileEditPopup)
 }
 
 const handleAddCard = (evt) => {
@@ -67,7 +79,7 @@ const handleAddCard = (evt) => {
     const imageUrl = formAddCard.link.value
 
     formAddCard.reset()
-    closePopup()
+    closePopup(profileAddPopup)
 
     return renderCard({
         name: placeName,
@@ -78,10 +90,10 @@ const handleAddCard = (evt) => {
 initialCards.map((card) => renderCard(card))
 
 profileEditButton.addEventListener('click', function () {
-    openPopup(profileEditPopup, overlayClickHandler, keyHandler)
+    openPopup(profileEditPopup, profileEditCloseButton, overlayClickHandler, keyHandler)
 })
 profileAddButton.addEventListener('click', function () {
-    openPopup(profileAddPopup)
+    openPopup(profileAddPopup, profileAddCloseButton, overlayClickHandler, keyHandler)
 })
 
 formProfileEdit.addEventListener('submit', handleProfileEdit)
